7ea858221604a397a0d3a4637df703fc9d9c0db8
RajeshGandru <Rajeshgandru.rg@gmail.com>
2022-09-07 17:50:39 +0530

    Initial Commit

---
+++ LNC_DriverUITests/LNC_DriverUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LNC_DriverUITestsLaunchTests.swift
+//  LNC_DriverUITests
+//
+//  Created by rajesh gandru on 07/09/22.
+//
+
+import XCTest
+
+class LNC_DriverUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
